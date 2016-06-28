f8b549465e2d6723544b7a8c4f4659518ea2dfce
nvdhau <nvdhau@gmail.com>
2016-06-28 23:04:56 +0700

    first commit

---
+++ messages.h
@@ -0,0 +1,11 @@
+// Scrolling messages
+
+const char string0[] PROGMEM = "PUSH YOUR HANDS UP !!! ";
+const char string1[] PROGMEM = ">>> eyeshineshine coming soon  ";
+const char string2[] PROGMEM = "eyeshineshine 2016 ";
+
+const char * const stringArray[] PROGMEM = {
+  string0,
+  string1,
+  string2
+};
